a1284fa49aa560203fb222a791ccfe1a0c902906
Benjamin Carter <webgoonie@gmail.com>
2023-02-27 01:13:40 -0500

    Updated Read Me

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ExpressApi
 This is a Powerful Express API set up using Sequelize
+To Run Environment Port Be sure to open them up first
+
+## How To Run? ##
+```sh
+npm start or yarn start
+```
+
+## To Use Environment Ports ##
+For Command Prompt: set PORT=5000
+For Power Shell: $env:PORT=5000
+For Bash (Windows): export PORT=5000
--- bin/run.js
+++ bin/run.js
@@ -8,11 +8,12 @@ const log = config.log();
 const app = require('../app')(config);
 
 const server = http.createServer(app);
+const PORT = process.env.PORT || 5005
 
-server.listen(process.env.PORT || 5005);
+server.listen(PORT);
 
 server.on('listening', () => {
   log.info(
-    `Hi there! I'm listening on port ${server.address().port} in ${app.get('env')} mode.`,
+    `Hi there! I'm listening on port ${server.address().port} in ${app.get('env')} mode.\nFollow link (Ctrl+Click): http://localhost:${PORT}`,
   );
 });
