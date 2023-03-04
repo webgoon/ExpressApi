99471daa71f46c7e4c15c326a18c15cc2e70cf63
Benjamin Carter <webgoonie@gmail.com>
2023-03-03 22:28:30 -0500

    Successful Models Working Before Associations

--- models/sequelize/index.js
+++ models/sequelize/index.js
@@ -60,18 +60,17 @@ module.exports = (sequelize) => {
         }
     );
 
-    const ContactInfo = sequelize.define('UserContactInfo', {
+    const UserContactInfo = sequelize.define('UserContactInfo', {
         id: {
             type: DataTypes.BIGINT,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
-            unique: true
+          
         },
         uid:{
             type: DataTypes.UUID,
             defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
             allowNull: false
         },
         phone: {
@@ -91,75 +90,128 @@ module.exports = (sequelize) => {
         }
     );
 
+    const NewsFeed = sequelize.define('NewsFeed', {
+        id: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        uid:{
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    },{
+        freezeTableName: true,
+        timestamps:true,
+        createdAt: true,
+        updatedAt: 'updatedlast',
+        paranoid: true
+    })
+
+    const Todo = sequelize.define('Todo', {
+        id: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        uid:{
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    },{
+        freezeTableName: true,
+        timestamps:true,
+        createdAt: true,
+        updatedAt: 'updatedlast',
+        paranoid: true
+    })
     
-        // const Employee = sequelize.define('Employee', {
-        //     id: {
-        //         type: DataTypes.BIGINT,
-        //         allowNull: false,
-        //         primaryKey: true,
-        //         autoIncrement: true,
-        //         unique: true
-        //     },
-        //     uid:{
-        //         type: DataTypes.UUID,
-        //         defaultValue: DataTypes.UUIDV4,
-        //         allowNull: true
-        //     },
-        //     firstName: {
-        //         type: DataTypes.STRING,
-        //         allowNull: true,
-        //         defaultValue: 'Jane',
-        //     },
-        //     lastName: {
-        //         type: DataTypes.STRING,
-        //         allowNull: true,
-        //         defaultValue: 'Doe'
-        //     },
-        //     email: {
-        //         type: DataTypes.STRING,
-        //         allowNull: false
-        //     }
-        //     },{
-        //         freezeTableName: true,
-        //         timestamps: true,
-        //         createdAt: true,
-        //         updatedAt: 'updatedlast',
-        //         paranoid: true
-        //     }
-        // );
+        const Employee = sequelize.define('Employee', {
+            id: {
+                type: DataTypes.BIGINT,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            uid:{
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                allowNull: true
+            },
+            firstName: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                defaultValue: 'Jane',
+            },
+            lastName: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                defaultValue: 'Doe'
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+            },{
+                freezeTableName: true,
+                timestamps: true,
+                createdAt: true,
+                updatedAt: 'updatedlast',
+                paranoid: true
+            }
+        );
     
-        // const EmployeeContactInfo = sequelize.define('EmployeeContactInfo', {
-        //     id: {
-        //         type: DataTypes.BIGINT,
-        //         allowNull: false,
-        //         primaryKey: true,
-        //         autoIncrement: true,
-        //         unique: true
-        //     },
-        //     uid:{
-        //         type: DataTypes.UUID,
-        //         defaultValue: DataTypes.UUIDV4,
-        //         primaryKey: true
-        //     },
-        //     phone: {
-        //         type: DataTypes.STRING,
-        //         allowNull: false
-        //     },
-        //     email: {
-        //         type: DataTypes.STRING,
-        //         allowNull: false
-        //     }
-        //     },{
-        //         freezeTableName: true,
-        //         timestamps:true,
-        //         createdAt: true,
-        //         updatedAt: 'updatedlast',
-        //         paranoid: true
-        //     }
-        // );
+        const EmployeeContactInfo = sequelize.define('EmployeeContactInfo', {
+            id: {
+                type: DataTypes.BIGINT,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            uid:{
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                allowNull: false
+            },
+            phone: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: true
+            }
+            },{
+                freezeTableName: true,
+                timestamps:true,
+                createdAt: true,
+                updatedAt: 'updatedlast',
+                paranoid: true
+            }
+        );
     
 
 
-sequelize.sync({alter: true});
+sequelize.sync();
     //sequelize.sync({force: true, match: /apiexpressdbDev$/}); // {force: true} {alter: true} {match: /apiexpressdbDev$/}
 }
